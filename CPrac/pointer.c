37da5fb6980fdd79929679419a657eae8e3a6d71
Sea-queue <cheng.qian@northeastern.edu>
2022-02-14 11:24:47 -0500

    combine pointers

--- CPrac/pointer.c
+++ CPrac/pointer.c
@@ -77,4 +77,18 @@ int main () {
        k = &j;
        printf("\nAddress of i = %u \n", *(k));
        printf("Value of i = %d \n", *(*k));
+
+
+       //more examples:
+       int age = 10;
+       int *page = &age;
+       double gpa = 4.0;
+       double *pgpa = &gpa;
+       char grade = 'A';
+       char *pgrade = &grade;
+       printf("%p\n", &age);
+       printf("%p\n", &gpa);
+       printf("%p\n", &grade);
+       printf("%p\n%p\n%p\n", &age, &gpa, &grade);
+       printf("%d\n%f\n%c\n%c\n", *page, *pgpa, *pgrade, *&grade);
 }
