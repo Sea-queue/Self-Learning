11d5d024489dbe4c5d651ee23312ea123ceb5937
Sea-queue <cheng.qian@northeastern.edu>
2022-03-09 19:34:12 -0500

    added a few cases

--- JavaPrac/basic/StrArrayList.java
+++ JavaPrac/basic/StrArrayList.java
@@ -3,56 +3,65 @@ import java.util.ArrayList;
 
 // This class demenstrate the similarities of
 // String  --  Array  -- List
-/*
- int to string:
-
- int i = 10;
- String s = String.valueOf(i);
- String s = Integer.toString(i);
-
- char to int:
-
- char c = '3';
- int i = Character.getNumericValue(c);
-
- */
 public class StrArrayList {
     public static void main(String[] args) {
 
         // String
-        //-------
+        // s.length()  |  s.charAt()
+        // String.valueOf(int)  |  Integer.toString(int)
+        // Character.getNumericValue(char)
         System.out.println("String related:");
 
-        String string = "Hello world";
-        System.out.println(string.length());
-        System.out.println(string.charAt(0)); //H
-        System.out.println(string.charAt(2)); //first l
+        String s = "Hello World";
+        System.out.println(s.length());     //11
+        System.out.println(s.charAt(0));    //H
+        System.out.println(s.charAt(6));    //W
+
+        int k = 10;
+        String result = String.valueOf(k);
+        System.out.println("string:" + result);
+
+        result = Integer.toString(k);
+        System.out.println("string:" + result);
+
+        char c = '3';
+        k = c;
+        System.out.println("c = " + k);
+        k = Character.getNumericValue(c);
+        System.out.println("c = " + k);
 
 
-        // Array related:
-        // --------------
+        // Array:
+        // array.length
         System.out.println("Array related:");
         //empty array
-        int[] intArray1 = {};
-        System.out.println(intArray1.length);
-
-        int[] intArray2 = new int[]{};
-        System.out.println(intArray2.length);
-
-        int[] intArray3 = new int[0];
-        System.out.println(intArray3.length);
+        int[] arr1 = new int[0];
+        System.out.println(arr1.length);
 
         //non-empty array:
-        int[] intArray4 = {1, 2, 3, 4, 5};
-        System.out.println(intArray4.length);
+        int[] arr2 = {1, 2, 3, 4, 5};
+        System.out.println(arr2.length);
 
-        for (int i = 0; i < intArray4.length; i += 1) {
-            System.out.print(intArray4[i] + " ");
+        for (int i = 0; i < arr2.length; i += 1) {
+            System.out.print(arr2[i] + " ");
         }
+
+        //2D array
+        int[][] arr3 = new int[10][20];
+        arr3[0][0] = 1;
+        System.out.println("\narr3[0][0] = " + arr3[0][0]);
+
+        //2D array
+        int[][] arr4 = { {1, 2}, {3, 4} };
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 2; j++)
+                System.out.println("arr4[" + i + "][" + j + "] = "
+                                   + arr4[i][j]);
 
 
         // List related:
-        // -------------
+        // list.add("value")  |  list.remove(idx)
+        // list.size()        |  list.get(ix)
         System.out.println("\n" + "List related:");
 
         List<Integer> intList = new ArrayList<>();
@@ -63,17 +72,5 @@ public class StrArrayList {
         System.out.println(intList.size());
         System.out.println(intList.get(0));
         System.out.println(intList.get(1));
-    }
-
-    /*
-     The string s will be shuffled such that the character at the ith
-     position moves to indices[i] in the shuffled string.
-     */
-    public String restoreString(String s, int[] indices) {
-        char[] result = new char[s.length()];
-        for (int i = 0; i < indices.length; i += 1) {
-            result[indices[i]] = s.charAt(i);
-        }
-        return new String(result);
     }
 }
