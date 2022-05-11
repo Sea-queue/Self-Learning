30f89486e446c3acd25a218b1471f5d27ffea67d
Sea-queue <86625937+Sea-queue@users.noreply.github.com>
2022-05-11 17:48:04 -0400

    changing the name

--- CPrac/r_w_a_file/append.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-int main(){
-    FILE * fpointer = fopen("cs240:\employees.txt", "a");
-    fprintf(fpointer, "overridden");
-    fclose(fpointer);
-    return 0;
-}
--- CPrac/r_w_a_file/file.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-int main(){
-    FILE * fpointer = fopen("cs240:\employees.txt", "w");
-    fprintf(fpointer, "Jim, professor\nSeaqueue, TA\n");
-    fclose(fpointer);
-    return 0;
-}
