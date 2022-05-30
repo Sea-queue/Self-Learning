e6c8f90ac482c8599e622a908723a24887dfed6b
Sea-queue <cheng.qian@northeastern.edu>
2022-05-30 15:33:32 -0400

    better understanding of list[::]

--- PythonPrac/basics/3list.py
+++ PythonPrac/basics/3list.py
@@ -1,6 +1,30 @@
 # Lists: ordered, mutable, allows duplicate elements
 # declared with [], accessed with []
 
+"""
+Given an integer array nums, reorder it such that
+nums[0] < nums[1] > nums[2] < nums[3]....
+
+You may assume the input array always has a valid answer.
+Example 1:
+Input: nums = [1,5,1,1,6,4]
+Output: [1,6,1,5,1,4]
+Explanation: [1,4,1,5,1,6] is also accepted.
+
+Example 2:
+Input: nums = [1,3,2,2,3,1]
+Output: [2,3,1,3,1,2]
+"""
+
+def wiggle(nums: list[int]) -> list[int]:
+    nums.sort()
+    half = len(nums[::2]) - 1
+    nums[::2], nums[1::2] = nums[half::-1], nums[:half:-1]
+    return nums
+print("wiggle:")
+print(wiggle([1,3,2,1,1,4]))
+
+
 # Empty list
 list1 = list()
 print(list1)
