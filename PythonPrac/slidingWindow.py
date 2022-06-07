7b7565810648df2f79d31f8e6bf8d466a8bdbff0
Sea-queue <cheng.qian@northeastern.edu>
2022-06-07 10:49:53 -0400

    another interesting case of substring

--- PythonPrac/slidingWindow.py
+++ PythonPrac/slidingWindow.py
@@ -109,3 +109,50 @@ print(maxSlidingWindow_1([1,3,7,-3,5,3,6,1], 3))
 print(maxSlidingWindow_1([1,-1], 1))
 print(maxSlidingWindow_2([1,3,7,-3,5,3,6,1], 3))
 print(maxSlidingWindow_2([1,-1], 1))
+
+
+"""
+Given two strings s and t of lengths m and n respectively,
+return the minimum window substring of s such that every character in t
+(including duplicates) is included in the window.
+If there is no such substring, return the empty string "".
+
+The testcases will be generated such that the answer is unique.
+A substring is a contiguous sequence of characters within the string.
+
+Example 1:
+Input: s = "ADOBECODEBANC", t = "ABC"
+Output: "BANC"
+Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
+
+Example 2:
+Input: s = "a", t = "a"
+Output: "a"
+Explanation: The entire string s is the minimum window.
+
+Example 3:
+Input: s = "a", t = "aa"
+Output: ""
+Explanation: Both 'a's from t must be included in the window.
+Since the largest window of s only has one 'a', return empty string.
+"""
+
+def minSub(s:str, t: str) -> str:
+    need, missing = Counter(t), len(t)
+    i = I = J = 0
+    for j, c in enumerate(s, 1):
+        missing -= need[c] > 0
+        need[c] -= 1
+        if not missing:
+            while i < j and need[s[i]] < 0:
+                need[s[i]] += 1
+                i += 1
+            if not J or J - I >= j - i:
+                I, J = i, j
+    return s[I:J]
+
+print(minSub("a", "a"))
+print(minSub("aa", "a"))
+print(minSub("", "n"))
+print(minSub("ADOBECODEBANCBA", "ABC"))
+print(minSub("ADOBECODEBANCBA", "ABCA"))
